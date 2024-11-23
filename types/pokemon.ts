8f3e2528f97b8b54a1dfade966a3ed3696cd0419
F.NS <faycal.nsm@gmail.com>
2024-11-23 14:43:22 +0100

    refactor(types): update pokemon type definitions with rest and graphql interfaces

--- types/pokemon.ts
+++ types/pokemon.ts
@@ -1,38 +1,82 @@
 export interface Pokemon {
+  id: number;
+  name: string;
+  sprites: PokemonSprites;
+  types: PokemonType[];
+  stats?: PokemonStat[];
+}
+
+export interface PokemonSprites {
+  front_default: string;
+}
+
+export interface PokemonType {
+  type: {
+    name: string;
+  };
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  stat: {
+    name: string;
+  };
+}
+
+// GraphQL specific types
+export interface PokemonListResponse {
+  pokemon_v2_pokemon: {
     id: number;
     name: string;
-    types: PokemonType[];
-    sprites: {
-      front_default: string;
-    };
-    stats: PokemonStat[];
-    species: {
-      url: string;
-    };
-  }
-  
-  export interface PokemonType {
-    type: {
-      name: string;
-    };
-    slot: number;
-  }
-  
-  export interface PokemonStat {
-    base_stat: number;
-    stat: {
-      name: string;
-    };
-  }
-  
-  export interface PokemonSpecies {
-    flavor_text_entries: {
-      flavor_text: string;
-      language: {
+    pokemon_v2_pokemonsprites: {
+      sprites: string;
+    }[];
+    pokemon_v2_pokemontypes: {
+      pokemon_v2_type: {
         name: string;
       };
     }[];
-    evolution_chain: {
-      url: string;
-    };
-  }
+  }[];
+}
+
+export interface PokemonDetailResponse {
+  pokemon_v2_pokemon: [
+    {
+      id: number;
+      name: string;
+      pokemon_v2_pokemonsprites: {
+        sprites: string;
+      }[];
+      pokemon_v2_pokemontypes: {
+        pokemon_v2_type: {
+          name: string;
+        };
+      }[];
+      pokemon_v2_pokemonstats: {
+        base_stat: number;
+        pokemon_v2_stat: {
+          name: string;
+        };
+      }[];
+    }
+  ];
+}
+
+// REST specific types
+export interface PokemonListRESTResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: {
+    name: string;
+    url: string;
+  }[];
+}
+
+export interface PokemonDetailRESTResponse {
+  id: number;
+  name: string;
+  sprites: PokemonSprites;
+  types: PokemonType[];
+  stats: PokemonStat[];
+}
