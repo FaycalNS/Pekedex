21e277fe3d25ce21ec040d19b72bbb0dabadbc87
F.NS <faycal.nsm@gmail.com>
2024-11-23 13:07:06 +0100

    refactor(api): reorganize lib structure for graphql and rest clients

--- components/ApolloProvider.tsx
+++ components/ApolloProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ApolloProvider as Provider } from "@apollo/client";
-import client from "@/lib/graphql/client";
+import client from "@/lib/api/graphql/client";
 
 export default function ApolloProvider({ children }: { children: React.ReactNode }) {
   return <Provider client={client}>{children}</Provider>;
